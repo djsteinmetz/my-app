397a0558286cad006dd0484dc43b692b5692b122
DJ Steinmetz <dsteinmetz@four51.com>
2021-03-15 17:56:45 -0500

    RRD-0001: Further sql work

--- src/pages/api/books/index.ts
+++ src/pages/api/books/index.ts
@@ -4,6 +4,6 @@ import sqlite from 'sqlite';
 
 export default async function getAllBooks(req: NextApiRequest, res: NextApiResponse): Promise<unknown> {
     const db = await sqlite.open('./mydb.sqlite');
-    const books = await db.all('select * from books');
+    const books = await db.all('select * from Users INNER JOIN Books ON Users.ID = Books.OwnerID');
     return res.json(books);
 }
